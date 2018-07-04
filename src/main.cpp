30b005b813a15f94c074fd10838a6df847f6fc0a
Petr Devaikin <petr.devaikin@daimler.com>
2018-07-04 21:28:32 +0200

    full screen

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@
 int main( ){
     ofGLFWWindowSettings settings;
     settings.setGLVersion(3, 2); //we define the OpenGL version we want to use
-    settings.width = 1024;
-    settings.height = 768;
+    settings.windowMode = OF_FULLSCREEN;
+    //settings.width = 1024;
+    //settings.height = 768;
     ofCreateWindow(settings);
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
